8aaa8f8e3697137a9dd13e9efd674bf4294e5721
Shruti Dixit <31957629+rudyerudite@users.noreply.github.com>
2020-08-05 11:06:24 +0530

    Create max-binary-heap.c

---
+++ max-binary-heap.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i=0,key, start, root, tmp = 0;
+    scanf("%d",&n);
+    int heap[n];
+    
+    while(i<n)
+    {
+        scanf("%d",&key);
+        heap[i] = key;
+        start = i;
+        root = (i-1)/2;
+        
+        while(heap[start] > heap[root])
+        {
+            tmp = heap[start];
+            heap[start] = heap[root];
+            heap[root] = tmp;
+            
+            start = root;
+            root = (start-1)/2;
+        }
+        
+        i++;
+    }
+    
+    for(i=0;i<n;i++)
+        printf("%d ",heap[i]);
+
+    return 0;
+}
